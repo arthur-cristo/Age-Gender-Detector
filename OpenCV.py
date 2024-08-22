2dc789a1d1a9300d15005d2c1df48e09d407cc83
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-08-22 12:33:08 -0300

    feat: improve predictions, and now them are made every 5 seconds

--- OpenCV.py
+++ OpenCV.py
@@ -1,47 +1,15 @@
 import cv2
+import time
 import numpy as np
 import dlib
 
-# TODO: Improve age prediction
+age_net = cv2.dnn.readNet("models/age_net.caffemodel", "models/age_deploy.prototxt")
+gender_net = cv2.dnn.readNet("models/gender_net.caffemodel", "models/gender_deploy.prototxt")
+frontal_face_detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
-
-
-def load_models():
-    try:
-        face_net = cv2.dnn.readNet("models/opencv_face_detector_uint8.pb", "models/opencv_face_detector.pbtxt")
-        age_net = cv2.dnn.readNet("models/age_net.caffemodel", "models/age_deploy.prototxt")
-        gender_net = cv2.dnn.readNet("models/gender_net.caffemodel", "models/gender_deploy.prototxt")
-        return face_net, age_net, gender_net
-    except cv2.error as e:
-        print("Error loading models:", e)
-        return None, None, None
-
-
-def preprocess_image(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    equalized = cv2.equalizeHist(gray)
-    smoothed = cv2.GaussianBlur(equalized, (5, 5), 0)
-    preprocessed = cv2.cvtColor(smoothed, cv2.COLOR_GRAY2BGR)
-    return preprocessed
-
-
-def detect_faces(face_net, image):
-    h, w = image.shape[:2]
-    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), [104, 117, 123], True, False)
-    face_net.setInput(blob)
-    detections = face_net.forward()
-    face_boxes = []
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > 0.7:
-            x1 = int(detections[0, 0, i, 3] * w)
-            y1 = int(detections[0, 0, i, 4] * h)
-            x2 = int(detections[0, 0, i, 5] * w)
-            y2 = int(detections[0, 0, i, 6] * h)
-            face_boxes.append((x1, y1, x2, y2))
-    return face_boxes
+AGE_LABELS = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+GENDER_LABELS = ['Homem', 'Mulher']
 
 
 def calculate_ear(eye):
@@ -55,87 +23,77 @@ def calculate_ear(eye):
 def is_looking_at_camera(left_eye, right_eye):
     left_ear = calculate_ear(left_eye)
     right_ear = calculate_ear(right_eye)
-    threshold = 0.26
+    threshold = 0.3
     average_ear = (left_ear + right_ear) / 2
-    return average_ear > threshold, average_ear
-
-
-def predict_age_gender(face, age_net, gender_net, MODEL_MEAN_VALUES):
-    blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-
-    gender_net.setInput(blob)
-    gender_preds = gender_net.forward()
-    gender = 'Homem' if gender_preds[0][0] > gender_preds[0][1] else 'Mulher'
-
-    age_net.setInput(blob)
-    age_preds = age_net.forward()
-    age_list = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-    age_confidences = age_preds[0]
-    age_index = np.argmax(age_confidences)
-    age_index = min(max(age_index, 0), len(age_list) - 1)
-    age = age_list[age_index]
-    age_confidence = age_confidences[age_index]
-
-    return gender, age, age_confidence
-
-
-def process_frame(frame, face_net, age_net, gender_net, MODEL_MEAN_VALUES):
-    h, w = frame.shape[:2]
-    preprocessed_frame = preprocess_image(frame)
-    face_boxes = detect_faces(face_net, preprocessed_frame)
-
-    if not face_boxes:
-        return frame
-
-    for (x1, y1, x2, y2) in face_boxes:
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), int(round(h / 150)), 8)
-        face = frame[y1:y2, x1:x2]
-        gender, age, age_confidence = predict_age_gender(face, age_net, gender_net, MODEL_MEAN_VALUES)
-        label = f"{gender}, Idade: {age} ({age_confidence * 100:.2f}%)"
-        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
-
-        gray_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-        faces_dlib = detector(gray_face)
-        if not faces_dlib:
-            cv2.putText(frame, "Nao esta olhando para a camera",
-                        (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-        for dlib_face in faces_dlib:
-            shape = predictor(gray_face, dlib_face)
-            landmarks = np.array([[p.x, p.y] for p in shape.parts()])
-            left_eye = landmarks[36:42]
-            right_eye = landmarks[42:48]
-            for (x, y) in np.concatenate((left_eye, right_eye)):
-                cv2.circle(frame, (x1 + x, y1 + y), 2, (0, 255, 0), -1)
-            looking_at_camera, ear_value = is_looking_at_camera(left_eye, right_eye)
-
-            if looking_at_camera:
-                cv2.putText(frame, "Esta olhando para a camera",
-                            (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-            else:
-                cv2.putText(frame, "Nao esta olhando para a camera",
-                            (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-    return frame
+    return average_ear > threshold
 
 
 def main():
-    face_net, age_net, gender_net = load_models()
-    if not face_net or not age_net or not gender_net:
-        return
-    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-    webcam = cv2.VideoCapture(0)  # Ensure the correct camera index
-    if not webcam.isOpened():
-        print("Error: Could not open webcam")
-        return
+    cap = cv2.VideoCapture(1)
+
+    start_time = time.time()
+    last_prediction = ""
+    last_looking_status = ""
     while True:
-        ret, frame = webcam.read()
+        ret, frame = cap.read()
         if not ret:
-            print("Error: Failed to grab frame")
             break
-        frame = process_frame(frame, face_net, age_net, gender_net, MODEL_MEAN_VALUES)
-        cv2.imshow("Age-Gender Detection", frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = frontal_face_detector(gray)
+
+        if not faces:
+            last_looking_status = "Rosto não detectado"
+            continue
+        for face in faces:
+            h, w = frame.shape[:2]
+
+            x1 = max(0, face.left() - 10)
+            y1 = max(0, face.top() - 10)
+            x2 = min(w, face.right() + 10)
+            y2 = min(h, face.bottom() + 10)
+
+            face_region = frame[y1:y2, x1:x2]
+            face_region = cv2.resize(face_region, (227, 227))
+            face_region = cv2.equalizeHist(cv2.cvtColor(face_region, cv2.COLOR_BGR2GRAY))
+            face_region = cv2.cvtColor(face_region, cv2.COLOR_GRAY2BGR)
+
+            shape = shape_predictor(gray, face)
+            landmarks = np.array([[p.x, p.y] for p in shape.parts()])
+            left_eye = landmarks[36:42]
+            right_eye = landmarks[42:48]
+            looking_at_camera = is_looking_at_camera(left_eye, right_eye)
+            last_looking_status = "Olhando para Camera" if looking_at_camera else "Nao Olhando para Camera"
+            elapsed_time = time.time() - start_time
+
+            if elapsed_time >= 5 and looking_at_camera:
+                start_time = time.time()
+
+                blob = cv2.dnn.blobFromImage(face_region, 1, (227, 227), (78.4263377603, 87.7689143744, 114.895847746),
+                                             swapRB=False)
+
+                gender_net.setInput(blob)
+                gender_preds = gender_net.forward()
+                gender = GENDER_LABELS[gender_preds[0].argmax()]
+
+                age_net.setInput(blob)
+                age_preds = age_net.forward()
+                age = AGE_LABELS[age_preds[0].argmax()]
+
+                last_prediction = f"Genero: {gender}, Idade: {age}"
+
+                print(last_prediction)
+
+            cv2.putText(frame, last_prediction, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
+            color = (0, 255, 0) if looking_at_camera else (0, 0, 255)
+            cv2.putText(frame, last_looking_status, (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2, cv2.LINE_AA)
+
+        cv2.imshow("Age and Gender Prediction", frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    webcam.release()
+
+    cap.release()
     cv2.destroyAllWindows()
 
 
